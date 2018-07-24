e5aced4d52d8a56ee20c006e25f4959119bb6684
jeanbarbosa9 <jeanbarbosa9@gmail.com>
2018-07-24 17:09:00 -0300

    Comentários no código

--- bw_package/views.py
+++ bw_package/views.py
@@ -15,12 +15,15 @@ configure_uploads(app, photos)
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST' and 'photo' in request.files:
+        #Salva a foto selecionada no site na pasta de uploads
         uploaded_image = photos.save(request.files['photo'])
-        #Como passar o caminho correto da imagem na linha abaixo ?
+        #Abre a foto salva na pasta de uploads
         novo_arquivo = Image.open('uploads/{}'.format(uploaded_image))
+        #Utiliza o algoritmo criado no arquivo converter.py para converter a imagem em preto e branco
         novo_arquivo = convert_bw(novo_arquivo)
         #Salva a imagem em preto e branco com o sufixo "bw" na frente do nome com que ela foi upada
         novo_arquivo.save('{}{}{}'.format('uploads/',"bw_" , uploaded_image))
+        #Salva uma variável com o mesmo nome da imagem que foi criada em preto e branco e passa essa variavel para o link do download da imagem
         bw_image = '{}{}'.format("bw_" , uploaded_image)
         return render_template('download.html', bw_image = bw_image)
     return render_template('upload.html')
